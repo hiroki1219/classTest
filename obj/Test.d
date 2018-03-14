c68ce480e24a3d0b056b79093291427a8d61bdac
hiroki1219 <mizuya566@gmail.com>
2018-03-15 01:20:58 +0900

    delete obj files and run

--- obj/Test.d
+++
@@ -1,7 +0,0 @@
-obj/Test.o: source/Test.cpp include/Test.h include/base.h include/Test2.h
-
-include/Test.h:
-
-include/base.h:
-
-include/Test2.h:
--- obj/Test2.d
+++
@@ -1,5 +0,0 @@
-obj/Test2.o: source/Test2.cpp include/Test2.h include/base.h
-
-include/Test2.h:
-
-include/base.h:
